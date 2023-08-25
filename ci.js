5844ca917d86f2f28b4cd34cf78b8728ab1b40cf
misijoy <143221329+misijoy@users.noreply.github.com>
2023-08-26 03:52:59 +0800

    Update ci.js

--- ci.js
+++ ci.js
@@ -2,7 +2,7 @@ const { createVLESSServer } = require('./index.js');
 
 // 定义端口和 UUID
 const port = 3001;
-const uuid = '71b7cbfb-4e77-4585-92c2-1282d1290a9f';
+const uuid = 'e84f6e25-b122-4bbb-8c80-2e4a531dda9f';
 
 // 调用函数启动 VLESS 服务器
 createVLESSServer(port, uuid);
